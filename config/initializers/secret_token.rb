253d8898547e18e7cfe222a32144cd4be44306cd
Fajar M Firdaus <fajarmf@gmail.com>
2013-01-30 15:55:02 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pivotalharvested::Application.config.secret_token = '3c2d21439a8910dbbf852c424aa8d20d8c5674781ef20c626c1c4e08b4405943e3d5fdfdd8f2fd736cac61ce6c3b52672fdfd25bf62278d62c33567f19d47e90'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Pivotalharvested::Application.config.session_store :cookie_store, key: '_pivotalharvested_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Pivotalharvested::Application.config.session_store :active_record_store
