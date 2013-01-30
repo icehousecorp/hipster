30729a9cf755db4e44ccc959eac2c27c535d3129
Fajar M Firdaus <fajarmf@gmail.com>
2013-01-30 16:35:40 +0700

    add validation to uniq integration

--- app/models/integration.rb
+++ app/models/integration.rb
@@ -1,4 +1,5 @@
 class Integration < ActiveRecord::Base
   belongs_to :user
   attr_accessible :harvest_project_id, :pivotal_project_id
+  validates :harvest_project_id, :uniqueness => { :scope => :pivotal_project_id, message: 'has been mapped' }
 end
