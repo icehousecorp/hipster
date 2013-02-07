a3426062e6263941fd389a4c5ecccd5d7d7dda59
fajar firdaus <idos@icehousecorp.com>
2013-02-07 14:30:49 +0700

    add harvest omniauth

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def callback
+	raise env['omniauth.auth'].inspect
+  end
 end
---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :harvest, 'lpU2PAFA1XDdrqMEQKE2hA==', 's4epXu9Rreu2vAApbOwnqiY+AwXqOm2oDcld5nlxY9iIE9yiUvaLtitkOBKJ1f201qvp5HzGDdqEjg72/4MwEQ=='
+end
