fbb722216e85e0270d0c1009e83422055c7325b9
Ronald Savianto <rsavianto@icehousecorp.com>
2013-03-08 18:00:03 +0700

    add email to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :username
+  attr_accessible :username, :email
   attr_accessible :harvest_id, :harvest_subdomain, :harvest_identifier, :harvest_secret
   attr_accessible :pivotal_id, :pivotal_token
 
@@ -12,6 +12,6 @@ class User < ActiveRecord::Base
   end
 
   def self.from_googleauth(auth)
-    create(username: auth['info']['name'])
+    create(username: auth['info']['name'], email: auth['info']['email'])
   end
 end
