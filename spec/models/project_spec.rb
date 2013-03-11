a89324b2b387c0316cc4a70613583e9ca9076bfb
Prildy <prildy@funamori-mp.local>
2013-03-11 17:44:19 +0700

    Update project spec, allow for mass assignment.

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -5,6 +5,21 @@ describe Project do
 	it {should have_many(:people).through(:person_mappings)}
 	it {should have_many(:person_mappings).dependent(:destroy)}
 	it {should belong_to :user}
+	it {should allow_mass_assignment_of(:harvest_project_id)}
+	it {should allow_mass_assignment_of(:pivotal_project_id)}
+	it {should allow_mass_assignment_of(:user_id)}
+	it {should allow_mass_assignment_of(:harvest_project_name)}
+	it {should allow_mass_assignment_of(:pivotal_project_name)}
+	it {should allow_mass_assignment_of(:project_name)}
+	it {should allow_mass_assignment_of(:client_id)}
+	it {should allow_mass_assignment_of(:client_name)}
+	it {should allow_mass_assignment_of(:harvest_project_code)}
+	it {should allow_mass_assignment_of(:harvest_billable)}
+	it {should allow_mass_assignment_of(:harvest_budget)}
+	it {should allow_mass_assignment_of(:pivotal_start_iteration)}
+	it {should allow_mass_assignment_of(:pivotal_start_date)}
+	it {should allow_mass_assignment_of(:person_ids)}
+	it {should allow_mass_assignment_of(:create_method)}
 
 	# it {should validate_presence_of(:harvest_email)}
 	# it {should validate_presence_of(:harvest_name)}
